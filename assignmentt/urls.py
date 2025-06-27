71f6fb5d0a21be93000173771ca161ec37a8fb6b
Premvkmishra <premvkmishra@gmail.com>
2025-06-28 00:11:24 +0530

    bakcend

--- assignmentt/urls.py
+++ assignmentt/urls.py
@@ -1,7 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
+  from django.http import JsonResponse
+  from django.urls import path, include
+  from django.contrib import admin
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('tasks.urls')),
-] 
+  def root(request):
+      return JsonResponse({"message": "Django ActiveTask API is running."})
+
+  urlpatterns = [
+      path('', root),
+      path('admin/', admin.site.urls),
+      path('api/', include('tasks.urls')),
+  ]
